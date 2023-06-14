7e2d089ee75671fbfdc0cc3412f1e75efb500af1
Kyle McCahill <kylemccahill@gmail.com>
2023-06-13 20:37:09 -0500

    added green alternating color

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,10 @@ const char* vertexShaderSource = "#version 330 core\n"
 	"}\0";
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
+	"uniform vec4 testColor;\n"
 	"void main()\n"
 	"{\n"
-	"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"FragColor = testColor;\n"
 	"}\n\0";
 
 int main()
@@ -127,7 +128,15 @@ int main()
 		// rendering
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		
 		glUseProgram(shaderProgram);
+
+		float timeValue = glfwGetTime();
+		float greenValue = sin(timeValue) / 2.0f + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "testColor");
+
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
